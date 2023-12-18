0459359e36f2d57dba7fddb921b2cd09aad66ff4
alergio <alejosmaya8@gmail.com>
2023-12-17 22:10:30 -0300

    First commit

---
+++ react-front/src/App.js
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+// importamos componentes
+import CompShowBlogs from "./blog/ShowBlog";
+import CompCreateBlog from "./blog/CreateBlog";
+import CompEditBlog from "./blog/EditBlog";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CompShowBlogs />} />
+          <Route path="/create" element={<CompCreateBlog />} />
+          <Route path="/edit/:id" element={<CompEditBlog />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
